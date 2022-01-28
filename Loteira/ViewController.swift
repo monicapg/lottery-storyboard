d1edd67cdb64dc1eef153e9824bfe6374bfd190e
Monica de Paula <monicapgomides@gmail.com>
2022-01-28 14:06:17 -0300

    finished app

--- Loteira/ViewController.swift
+++ Loteira/ViewController.swift
@@ -6,14 +6,82 @@
 //
 
 import UIKit
+import Foundation
+
+enum GameType: String {
+    
+    case megasena = "Mega-Sena"
+    case quina = "Quina"
+}
+
+infix operator >-<
+func >-< (total: Int, universe: Int) -> [Int] {
+    var result : [Int] = []
+    while result.count < total {
+        let randomNumber = Int(arc4random_uniform(UInt32(universe))+1)
+        if !result.contains(randomNumber) {
+            result.append(randomNumber)
+        }
+    }
+    return result.sorted()
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbGameType: UILabel!
+    @IBOutlet weak var scGameType: UISegmentedControl!
+    @IBOutlet var balls: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //showNumbers(for: .megasena)
+        
+    }
+    
+    func showNumbers(for type: GameType) {
+        lbGameType.text = type.rawValue
+        var game: [Int] = []
+        switch type {
+        case .megasena:
+            game = 6>-<60
+            balls.last!.isHidden = false
+        case .quina:
+            game = 5>-<80
+            balls.last!.isHidden = true
+        }
+
+        for (index, game) in game.enumerated() {
+            balls[index].setTitle("\(game)", for: .normal)
+        }
+
+    }
+    
+    @IBAction func generateTheGame() {
+        switch scGameType.selectedSegmentIndex {
+        case 0:
+            showNumbers(for: .megasena)
+        default:
+            showNumbers(for: .quina)
+        }
+    }
+    
+    @IBAction func tappedSegmentedControl(_ sender: UISegmentedControl) {
+        
+        switch scGameType.selectedSegmentIndex {
+        case 0:
+            balls.last!.isHidden = false
+            initializeBalls(number: 60)
+        default:
+            balls.last!.isHidden = true
+            initializeBalls(number: 80)
+        }
     }
 
-
+    func initializeBalls(number: Int) {
+        let numberAsString = String(number)
+        
+        for index in 0...5 {
+            balls[index].setTitle(numberAsString, for: .normal)
+        }
+    }
 }
-
